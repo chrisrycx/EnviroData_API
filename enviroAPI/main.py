25309f69df6c8864ab91789bd4433bcffe05543a
Chris Cox <chrisc@dyacon.com>
2021-09-23 12:37:01 -0600

    Basic data check complete

--- enviroAPI/main.py
+++ enviroAPI/main.py
@@ -2,26 +2,38 @@
 EnviroData QC API Demo
 '''
 from typing import Optional, List
-
+from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
+import envirodataqc
+import pandas as pd
 
 
 class Temperature(BaseModel):
+    auth_token: str
     dataids: List[int]
-    dtstamps: List[str]
+    dtstamps: List[datetime]
     values: List[float]
 
 app = FastAPI()
 
 @app.post("/checkdata/2m_air_temperature")
 async def loadT(T:Temperature):
-    mid = T.dataids
-    dt = T.dtstamps
-    val = T.values 
+    #Load data into a pandas DF
+    dts = pd.DatetimeIndex(T.dtstamps)
+    data = pd.DataFrame(
+        {
+            'data_id':T.dataids,
+            'values':T.values
+        },
+        index=dts
+    )
 
-    return {
-        "message": "working",
-        "Temperature":dt,
-        "Value":val
-        }
+    #Check data
+    data_flagged = envirodataqc.check_vals(data['values'],'air_temperature')
+    print(data_flagged.head())
+
+    #Convert to dictionary
+    data_flagged_json = data_flagged.to_json()
+
+    return data_flagged_json
