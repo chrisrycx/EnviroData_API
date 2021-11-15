7ca71e32e8d3e5754632a71794f0580ba6fafae3
Chris Cox <chrisc@dyacon.com>
2021-11-15 13:18:07 -0700

    Successfully tested plotly

--- enviroAPI/static/js/enviroAPI.js
+++ enviroAPI/static/js/enviroAPI.js
@@ -1,142 +1,91 @@
-//Javascript for interacting with API
+//Test plotly
 
 //Event handler
 var inputform = document.getElementById('datainput')
 inputform.onsubmit = test
 
+TESTER = document.getElementById('tester');
+
+var dts = [
+    '2021-03-07 00:50',
+    '2021-03-07 01:00',
+    '2021-03-07 01:10',
+    '2021-03-07 01:20',
+    '2021-03-07 01:30',
+    '2021-03-07 01:40',
+    '2021-03-07 01:50',
+    '2021-03-07 02:00',
+    '2021-03-07 02:10',
+    '2021-03-07 02:20',
+    '2021-03-07 02:30',
+    '2021-03-07 02:40',
+    '2021-03-07 02:50'
+]
+
+var y = [3.9,5.1,5.9,5.1,5.1,5.1,5.1,5.7,-16.2,6.7,5.8,-6.7,26.5]
+
+var quality = [0,0,0,1,1,1,1,0,2,0,0,1,2]
+
+Plotly.newPlot( TESTER, [{
+    x: dts,
+    y: y }], {
+    margin: { t: 0 } } 
+);
+
 //Functions
 function test(event){
     event.preventDefault()
-    //console.log(this[0].value)
-    let data = new FormData(this)
-    for(const v of data){
-        console.log(v)
+    console.log("in test function")
+
+    //Create line for all data
+    let dataline = {
+        x: dts,
+        y: y,
+        mode: 'lines',
+        showlegend: false
     }
+
+    //Extract suspicious and bad data points
+    dts_susp = []
+    dts_bad = []
+    y_susp = []
+    y_bad = []
+    for(let i = 0; i < quality.length; i++){
+        if(quality[i] == 1){
+            dts_susp.push(dts[i])
+            y_susp.push(y[i])
+        }else if(quality[i] == 2){
+            dts_bad.push(dts[i])
+            y_bad.push(y[i])
+        }
+    }
+
+    //Create markers for susp/bad
+    let points_susp = {
+        x: dts_susp,
+        y: y_susp,
+        mode: 'markers',
+        name: 'Suspicious',
+        marker: {
+            color: 'yellow',
+            size: '5'
+        }
+    }
+
+    let points_bad = {
+        x: dts_bad,
+        y: y_bad,
+        mode: 'markers',
+        name: 'Bad',
+        marker: {
+            color: 'red',
+            size: '5'
+        }
+    }
+
+
+    Plotly.newPlot(TESTER,[dataline,points_susp,points_bad]);
+    
 }
 
-//Try a bokeh plot
-const plt = Bokeh.Plotting
-const x = Bokeh.LinAlg.linspace(-0.5, 20.5, 10);
-const y = x.map(function (v) { return v * 0.5 + 3.0; });
-const source = new Bokeh.ColumnDataSource({ data: { x: x, y: y } });
 
-const Tplot = plt.figure({
-    plot_width: 500,
-    plot_height: 350
-})
-
-const myline = Tplot.line({
-    source: source,
-    x: 'x',
-    y: 'y'
-})
-
-plt.show(Tplot)
-
-
-/*
-#Build Bokeh ColumnDataSource from default data
-    #Also convert default data to string for loading
-    #to textarea
-    data = {'dt':[],'T':[],'quality':[]}
-    datastr = ''
-    for row in testdata:
-        rowdt = datetime.strptime(row[0],'%Y-%m-%d %H:%M')
-        data['dt'].append(rowdt)
-        data['T'].append(row[1])
-        data['quality'].append(row[2])
-
-        rowstr = ','.join([row[0],str(row[1])]) + '\n'
-        datastr = datastr + rowstr
-
-    #Delete last \n character
-    datastr = datastr[:-1]
-
-    dsource = ColumnDataSource(data=data)
-    filter_mod = [True if y_val == 1 else False for y_val in dsource.data['quality']]
-    filter_bad = [True if y_val == 2 else False for y_val in dsource.data['quality']]
-    mod_view = CDSView(source=dsource, filters=[BooleanFilter(filter_mod)])
-    bad_view = CDSView(source=dsource, filters=[BooleanFilter(filter_bad)])
-
-    #Create the text area
-    textarea = TextAreaInput(value=datastr, rows=10)
-
-    #Load data callback
-    loaddataJS = CustomJS(args={'textarea':textarea,'chartdata':dsource},code="""
-    //Test parsing data from input
-    let txtinput = textarea.value;
-
-    //Split by row
-    let rows = txtinput.split(/\\r?\\n/);
-
-    //Extract data by row
-    let dts = []
-    let vals = []
-    for(let row of rows){
-        let data = row.split(',')
-        dts.push(data[0])
-        vals.push(data[1])
-    }
-    let dataset = {
-        dtstamps: dts,
-        values: vals
-    }
-
-    fetch('/checkdata/2m_air_temperature',{
-            method: 'POST',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(dataset)
-        })
-        .then(response => response.json())
-        .then(data => {
-            //JS to update chart
-            console.log('In final function')
-            console.log(data)
-            console.log(chartdata)
-            chartdata.data['T'] = [5,5,5,5,5,5,5,5,5,5,5,5,5]
-            chartdata.data['quality'] = [2,1,2,1,2,1,2,1,2,1,2,1,2]
-            chartdata.change.emit()
-        })
-
-    """)
-
-
-    #Create a button and link to a callback
-    button = Button(label='Check Data')
-    button.js_on_click(loaddataJS)
-
-    #Generate initial Bokeh plot
-    Tplot = figure(
-            plot_width=500,
-            plot_height=350,
-            x_axis_type='datetime',
-            toolbar_location=None)
-
-    #Add a line for the data
-    Tplot.line(source=dsource,x='dt',y='T')
-
-    #Add circles to mark suspicious data points
-    Tplot.circle(
-        source=dsource,
-        view=mod_view,
-        x='dt',
-        y='T',
-        color='yellow',
-        size=8,
-        legend_label='Suspicious'
-        )
-
-    #Add x to mark bad data points
-    Tplot.circle_x(
-        source=dsource,
-        view=bad_view,
-        x='dt',
-        y='T',
-        color='red',
-        line_color='black',
-        size=8,
-        legend_label='Bad'
-        )
-
-    script, divs = components([Tplot,textarea,button])
-    */
